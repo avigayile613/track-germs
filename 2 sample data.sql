772dfcf84f29466d1169df21bf993d94e22ea914
77ahuva <77ahuva@gmail.com>
2025-11-29 23:36:42 -0500

    added insert statement to sample data

--- 2 sample data.sql
+++ 2 sample data.sql
@@ -1,6 +1,10 @@
+delete germ
+
+insert germ(GermName, Host)
 select 'Norovirus', 'human'
 select 'Salmonella', 'plant'
 select 'Clostridium perfringens', 'human'
 select 'Campylobacter', 'animal'
 select 'Staphylococcus aureus', 'human'
+
 
